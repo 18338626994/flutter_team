835fc2a44d58889e227e22d424edceeb6f5fc985
yunfei <861860170@qq.com>
2020-03-02 18:11:19 +0800

    flutter pub get

--- .ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
+++ .ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
@@ -4,9 +4,37 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<flutter_boost/FlutterBoostPlugin.h>)
+#import <flutter_boost/FlutterBoostPlugin.h>
+#else
+@import flutter_boost;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<image_picker/ImagePickerPlugin.h>)
+#import <image_picker/ImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBoostPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBoostPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
 }
 
 @end
